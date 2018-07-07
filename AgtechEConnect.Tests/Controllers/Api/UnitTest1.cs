e0334e95d0b5ff6cc043988502c3915cb9b51b93
Alejandro Lazo Hernandez <Alejandro.Lazo@openlink.com>
2018-07-06 22:24:36 -0400

    Add unit tests for Contracts API Controller

--- AgtechEConnect.Tests/Controllers/Api/UnitTest1.cs
+++ AgtechEConnect.Tests/Controllers/Api/UnitTest1.cs
@@ -1,8 +1,14 @@
 ﻿using AgtechEConnect.Controllers.Api;
 using AgtechEConnect.Core;
+using AgtechEConnect.Core.Dtos.Contract;
+using AgtechEConnect.Core.Models;
+using AgtechEConnect.Core.Repositories;
 using AgtechEConnect.Tests.Extensions;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
+using System.Web.Http.Results;
 
 namespace AgtechEConnect.Tests.Controllers.Api
 {
@@ -10,19 +16,44 @@ namespace AgtechEConnect.Tests.Controllers.Api
 	public class ContractsControllerTest
 	{
 		private ContractsController _controller;
+		private Mock<IContractRepository> _mockRepository;
 
 		public ContractsControllerTest()
 		{
+			_mockRepository = new Mock<IContractRepository>();
 			var mockUoW = new Mock<IUnitOfWork>();
 
-			//Object is the actual implementation of UnitOfWork
+			mockUoW.SetupGet(u => u.Contracts).Returns(_mockRepository.Object);
+
 			_controller = new ContractsController(mockUoW.Object);
 			_controller.MockCurrentUser("1", "user1@domain.com");
+			_controller.Request = new System.Net.Http.HttpRequestMessage()
+			{
+				RequestUri = new Uri("http://localhost:64979/api/contracts/1")
+			};
 		}
 
 		[TestMethod]
-		public void TestMethod1()
+		public void Get_NotValidId_ReturnNotFound()
 		{
+			var result = _controller.Get(-111);
+
+			result.Should().BeOfType<NotFoundResult>();
+		}
+
+		[TestMethod]
+		public void Get_ValidId_ReturnOk()
+		{
+			var contract = new Contract()
+			{
+				ContractId = 1
+			};
+
+			_mockRepository.Setup(r => r.Get(1)).Returns(contract);
+
+			var result = _controller.Get(1);
+
+			result.Should().BeOfType<OkNegotiatedContentResult<ContractDto>>();
 		}
 	}
 }
--- AgtechEConnect/Controllers/Api/ContractsController.cs
+++ AgtechEConnect/Controllers/Api/ContractsController.cs
@@ -1,6 +1,7 @@
 ﻿using AgtechEConnect.Core;
 using AgtechEConnect.Core.Dtos.Contract;
 using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -34,19 +35,62 @@ namespace AgtechEConnect.Controllers.Api
 
 			foreach (var c in contracts)
 			{
+				var url = string.Empty;
+
+				try
+				{
+					url = UrlHelper.Link("DefaultApi", new { id = c.ContractId });
+				}
+				catch (Exception)
+				{
+					url = string.Empty;
+				}
+
 				dto.Add(new ContractDto
 				{
-					Url = UrlHelper.Link("DefaultApi", new { id = c.ContractId }),
+					Url = url,
 					ContractId = c.ContractId,
 					ContractNo = c.ContractNo,
 					CommodityId = c.CommodityId,
 					ContractDate = c.ContractDate,
 					LocationId = c.LocationId,
-					VendorCustomerId = c.VendorCustomerId,
+					VendorCustomerId = c.VendorCustomerId
 				});
 			}
 
 			return dto;
+		}
+
+		public IHttpActionResult Get(int Id)
+		{
+			var contractInDb = _unitOfWork.Contracts.Get(Id);
+
+			if (contractInDb == null)
+				return NotFound();
+
+			var url = string.Empty;
+
+			try
+			{
+				url = UrlHelper.Link("DefaultApi", new { id = contractInDb.ContractId });
+			}
+			catch (Exception)
+			{
+				url = string.Empty;
+			}
+
+			var contractDto = new ContractDto()
+			{
+				Url = url,
+				ContractId = contractInDb.ContractId,
+				ContractNo = contractInDb.ContractNo,
+				CommodityId = contractInDb.CommodityId,
+				ContractDate = contractInDb.ContractDate,
+				LocationId = contractInDb.LocationId,
+				VendorCustomerId = contractInDb.VendorCustomerId
+			};
+
+			return Ok(contractDto);
 		}
 
 		public UrlHelper UrlHelper //Using defered execution. At this point the request should exist. 
